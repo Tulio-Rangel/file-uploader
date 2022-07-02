ded4079100be840f9cd375e5036f9b5e14086b68
Tulio-Rangel <tjrn20@hotmail.com>
2022-07-02 18:21:31 -0500

    File loader working

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: function (req, file, cb) {
+          cb(null, file.originalname + '_' + Date.now() + '.csv');
+        },
+      }),
+    }),
+  )
+  @Post('file')
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return {
+      msg: `File ${file.filename} uploaded successfully`,
+    };
   }
 }
